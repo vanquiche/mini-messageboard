22b25a852742272134aa057d1fd5db8c023dbdc7
vanquiche <stevevang2010@hotmail.com>
2021-07-18 18:00:06 -0700

    add routes to new message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+const moment = require('moment');
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: moment().format('MMMM Do, h:mm:ss a'),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: moment().format('MMMM Do, h:mm:ss a'),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  res.render('index', { title: 'Message Board', messages: messages });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form', { title: 'New Message' });
+});
+
+router.post('/new', (req, res) => {
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: moment().format('MMMM Do, h:mm:ss a'),
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
